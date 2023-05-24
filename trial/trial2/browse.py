d533c95d118858861d0d1f8c96e9874744cffd4f
ulrikSebastienR <rastogisaurab29@gmail.com>
2023-05-24 14:36:09 +0530

    pour deletion

--- trial/trial2/browse.py
+++ trial/trial2/browse.py
@@ -4,18 +4,20 @@ os.chdir("/home/normal/python_thoughtful/trial/")
 print(f"current pwd is {os.getcwd()}")
 
 for root, dirs, files in os.walk(os.getcwd()):
-    print(root)
-    print(list(dirs))
-    print(list(files))
+    print(f"root is {root}")
+    print(f"in {root} directories are {list(dirs)}")
+    print(f"in {root} files are {list(files)}")
 # browses the tree perfectly, lets join paths
+print("\n", "# browses the tree perfectly, lets join paths", "\n")
 
 for root, dirs, files in os.walk(os.getcwd()):
     print(f"root is {root}")
     for d in dirs:
         print(f"joined root and d is {os.path.join(root, d)}")
+        print(f"files in {root} are {list(files)}")
         #correct so far
         for file in files:
-            print("file is {file}")
+            print(f"file is {file}")
             print(f"joined at file level is {os.path.join(root,d,file)}")
 
 
