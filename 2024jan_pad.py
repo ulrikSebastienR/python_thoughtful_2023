9354d731b52c704c27277dfed4eb5188e2acea09
ulrikSebastienR <rastogisaurab29@gmail.com>
2024-02-04 12:56:37 +0530

    can't add another folder to git without unlinking it first

--- 2024jan_pad.py
+++ 2024jan_pad.py
@@ -1,7 +1,11 @@
+#INCOMPLETE sort a list without using sorted or sort function
+#INCOMPLETE PROGRAMS ARE AT THE TOP
+#LEARNT #control b opens module browser in a program
+
 #max without using max function
 #lcm array
 #generate list containing random integers
-#control b opens module browser in a program
+
 
 import random
 
@@ -15,15 +19,23 @@ def lcm_array(a=[2,3,4]):
         if item == a[-1]:
             return i
 
-##l = []                
-##def fibb(x=1,y=1):
-##    y,x = x+y, y
-##    return x,y
-##for i in range(5):
-##    l.append(fibb(x,y)[1])
+#fibb in a list
+def lfibbf(n):
+    l = []
+    if n<=0:
+        return "terms can not be negative"
+    elif n==1 or n==2:
+        l.append(1)
+    else:
+        l.append(lfibbf(n-1)+lfibbf(n-2))
+    return l #[[[[1, 1], 1], [1, 1]]]
+    
+
+
+#lambda, map and filter        
         
-l = []
-l.extend(random.randint(1,50) for i in range(5))
+
+    
         
         
     
@@ -31,15 +43,22 @@ l.extend(random.randint(1,50) for i in range(5))
 
 #max
 rl = [random.randint(1,90) for i in range(10)] #create a list
-sl = [rl[0]]
+sl = rl[0]
 for item in rl:
-    if item > sl[-1]:
-        sl.append(item)
+    if item > sl:
+        sl= item
+print(sl, max(rl), rl)        
 
-#dict
-t = (2,'a'),(3,'b')
-d = dict(t)
-    
+import os
+print(os.getcwd())
+os.chdir("/home/normal/python_thoughtful_2023/dir_pour_experimentation")
+print(list(os.walk(os.getcwd()))) #gives direct tuple
 
-    
-
+#fibb recursion
+def fibb(n):
+    if n<=0:
+        return "please enter positive n"
+    elif n==1 or n==2:
+        return 1
+    else:
+        return fibb(n-1)+fibb(n-2)
